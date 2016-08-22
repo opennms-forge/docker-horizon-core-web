99eb89ce88643550f67b09efc611c0bb1e832aa4
Ronny Trommer <ronny@opennms.org>
2016-08-22 10:40:32 +0200

    Add Volumes for storing data outside of the container

--- Dockerfile
+++ Dockerfile
@@ -2,23 +2,38 @@ FROM indigo/centos-jdk8:latest
 MAINTAINER Ronny Trommer <ronny@no42.org>
 
 ENV OPENNMS_VERSION branches-release-18.0.1
-ENV DB_NAME opennms
-ENV DB_USER opennms
-ENV DB_PASS opennms
+
+ENV POSTGRES_USER "postgres"
+ENV POSTGRES_PASSWORD "postgres"
+
+ENV OPENNMS_DBUSER "opennms"
+ENV OPENNMS_DBPASS "opennms"
+
+ENV OPENNMS_HOME "/opt/opennms"
+ENV OPENNMS_DB_CONFIG "${OPENNMS_HOME}/etc/opennms-datasources.xml"
 
 RUN rpm -Uvh http://yum.opennms.eu/repofiles/opennms-repo-${OPENNMS_VERSION}-rhel7.noarch.rpm && \
     rpm --import http://yum.opennms.org/OPENNMS-GPG-KEY && \
     yum -y install opennms-core && \
     yum -y install opennms-webapp-jetty
 
+COPY ./docker-entrypoint.sh /
+
+## Volumes for storing data outside of the container
+VOLUME ["/opt/opennms/etc","/var/log/opennms","/var/opennnms"]
+
+ENTRYPOINT [ "/docker-entrypoint.sh" ]
+
+CMD [ "-h" ]
+
 ##------------------------------------------------------------------------------
 ## EXPOSED PORTS
 ##------------------------------------------------------------------------------
-## -- OpenNMS HTTP       Port(8980)
-## -- OpenNMS JMX        Port(18980)
-## -- OpenNMS KARAF RMI  Port(1099)
-## -- OpenNMS KARAF SSH  Port(8101)
-## -- OpenNMS MQ         Port(61616)
-## -- OpenNMS Eventd     Port(5817)
-## -- SNMP (SERVER)      Port(162)
-EXPOSE 8980 18980 1099 8101 61616 5817 162
+## -- OpenNMS HTTP       8980/TCP
+## -- OpenNMS JMX        18980/TCP
+## -- OpenNMS KARAF RMI  1099/TCP
+## -- OpenNMS KARAF SSH  8101/TCP
+## -- OpenNMS MQ         61616/TCP
+## -- OpenNMS Eventd     5817/TCP
+## -- SNMP (SERVER)      162/UDP
+EXPOSE 8980 18980 1099 8101 61616 5817 162/udp
---
+++ docker-entrypoint.sh
@@ -0,0 +1,84 @@
+#!/bin/bash -e
+# =====================================================================
+# Build script running OpenNMS in Docker environment
+#
+# Source: https://github.com/indigo423/docker-opennms
+# Web: https://www.opennms.org
+#
+# =====================================================================
+
+# Error codes
+E_ILLEGAL_ARGS=126
+
+# Help function used in error messages and -h option
+usage() {
+    echo ""
+    echo "Docker entry script for OpenNMS service container"
+    echo ""
+    echo "-i: Initialize database and Java environment"
+    echo "-s: Start OpenNMS core monitoring and webapplication services"
+    echo "-h: Show this help"
+    echo ""
+}
+
+initdb() {
+    if [ ! -d ${OPENNMS_HOME} ]; then
+        echo "OpenNMS home directory doesn't exist in ${OPENNMS_HOME}"
+        exit
+    fi
+
+    sed -i "s,url=\"jdbc:postgresql:\/\/localhost:5432\/template1\",url=\"jdbc:postgresql:\/\/${PSQL_SERVICE_PORT_5432_TCP_ADDR}:${PSQL_SERVICE_PORT_5432_TCP_PORT}\/template1\"," ${OPENNMS_DB_CONFIG}
+    sed -i "s,user-name=\"postgres\",user-name=\"${POSTGRES_USER}\"," ${OPENNMS_DB_CONFIG}
+    sed -i "s,password=\"\",password=\"${POSTGRES_PASSWORD}\"," ${OPENNMS_DB_CONFIG}
+
+    sed -i "s,url=\"jdbc:postgresql:\/\/localhost:5432\/opennms\",url=\"jdbc:postgresql:\/\/${PSQL_SERVICE_PORT_5432_TCP_ADDR}:${PSQL_SERVICE_PORT_5432_TCP_PORT}\/opennms\"," ${OPENNMS_DB_CONFIG}
+    sed -i "s,user-name=\"opennms\",user-name=\"${OPENNMS_DBUSER}\"," ${OPENNMS_DB_CONFIG}
+    sed -i "s,password=\"opennms\",password=\"${OPENNMS_DBPASS}\"," ${OPENNMS_DB_CONFIG}
+
+    cd ${OPENNMS_HOME}/bin
+    ./runjava -s
+    ./install -dis
+}
+
+start() {
+    cd ${OPENNMS_HOME}/bin
+    exec ./opennms -f start
+}
+
+# Evaluate arguments for build script.
+if [[ "${#}" == 0 ]]; then
+    usage
+    exit ${E_ILLEGAL_ARGS}
+fi
+
+# Evaluate arguments for build script.
+while getopts his flag; do
+    case ${flag} in
+        h)
+            usage
+            exit
+            ;;
+        i)
+            initdb
+            exit
+            ;;
+        s)
+            start
+            exit
+            ;;
+        *)
+            usage
+            exit ${E_ILLEGAL_ARGS}
+            ;;
+    esac
+done
+
+# Strip of all remaining arguments
+shift $((OPTIND - 1));
+
+# Check if there are remaining arguments
+if [[ "${#}" > 0 ]]; then
+    echo "Error: To many arguments: ${*}"
+    usage
+    exit ${E_ILLEGAL_ARGS}
+fi
