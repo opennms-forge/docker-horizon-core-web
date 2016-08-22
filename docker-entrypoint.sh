537209cac2a4b6d0ef52f654a7d127a430a957ab
Ronny Trommer <ronny@opennms.org>
2016-08-23 00:35:09 +0200

    Add flag to initialize db only when configured file is not present

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,38 +17,42 @@ usage() {
     echo ""
     echo "Docker entry script for OpenNMS service container"
     echo ""
-    echo "-f: Initialize/Update the database and start OpenNMS in forground in one step"
-    echo "-h: Show this help"
-    echo "-i: Initialize/Update database and Java environment"
-    echo "-s: Start OpenNMS core monitoring and webapplication services"
+    echo "-f: Initialize as in -i and start OpenNMS in forground."
+    echo "-h: Show this help."
+    echo "-i: Initialize Java environment, database and pristine OpenNMS configuration files."
+    echo "-s: Start OpenNMS core monitoring and web application services."
     echo ""
 }
 
 initdb() {
     if [ ! -d ${OPENNMS_HOME} ]; then
-        echo "OpenNMS home directory doesn't exist in ${OPENNMS_HOME}"
-        exit
+        echo "OpenNMS home directory doesn't exist in ${OPENNMS_HOME}."
+        exit ${E_ILLEGAL_ARGS}
     fi
 
-    sed -i "s,url=\"jdbc:postgresql:\/\/localhost:5432\/template1\",url=\"jdbc:postgresql:\/\/${POSTGRES_HOST}:${POSTGRES_PORT}\/template1\"," ${OPENNMS_DB_CONFIG}
-    sed -i "s,user-name=\"postgres\",user-name=\"${POSTGRES_USER}\"," ${OPENNMS_DB_CONFIG}
-    sed -i "s,password=\"\",password=\"${POSTGRES_PASSWORD}\"," ${OPENNMS_DB_CONFIG}
+    if [ ! -f ${OPENNMS_HOME}/etc/configured ]; then
+        sed -i "s,url=\"jdbc:postgresql:\/\/localhost:5432\/template1\",url=\"jdbc:postgresql:\/\/${POSTGRES_HOST}:${POSTGRES_PORT}\/template1\"," ${OPENNMS_DB_CONFIG}
+        sed -i "s,user-name=\"postgres\",user-name=\"${POSTGRES_USER}\"," ${OPENNMS_DB_CONFIG}
+        sed -i "s,password=\"\",password=\"${POSTGRES_PASSWORD}\"," ${OPENNMS_DB_CONFIG}
 
-    sed -i "s,url=\"jdbc:postgresql:\/\/localhost:5432\/opennms\",url=\"jdbc:postgresql:\/\/${POSTGRES_HOST}:${POSTGRES_PORT}\/${OPENNMS_DBNAME}\"," ${OPENNMS_DB_CONFIG}
-    sed -i "s,user-name=\"opennms\",user-name=\"${OPENNMS_DBUSER}\"," ${OPENNMS_DB_CONFIG}
-    sed -i "s,password=\"opennms\",password=\"${OPENNMS_DBPASS}\"," ${OPENNMS_DB_CONFIG}
+        sed -i "s,url=\"jdbc:postgresql:\/\/localhost:5432\/opennms\",url=\"jdbc:postgresql:\/\/${POSTGRES_HOST}:${POSTGRES_PORT}\/${OPENNMS_DBNAME}\"," ${OPENNMS_DB_CONFIG}
+        sed -i "s,user-name=\"opennms\",user-name=\"${OPENNMS_DBUSER}\"," ${OPENNMS_DB_CONFIG}
+        sed -i "s,password=\"opennms\",password=\"${OPENNMS_DBPASS}\"," ${OPENNMS_DB_CONFIG}
 
-    cd ${OPENNMS_HOME}/bin
-    ./runjava -s
-    sleep ${START_DELAY}
-    ./install -dis
+        cd ${OPENNMS_HOME}/bin
+        ./runjava -s
+        sleep ${START_DELAY}
+        ./install -dis
+    else
+        echo "OpenNMS is already configured skip initdb."
+    fi
 }
 
 initConfig() {
     if [ ! "$(ls -A ${OPENNMS_HOME}/etc)"  ]; then
         cp -r ${OPENNMS_HOME}/share/etc-pristine/* ${OPENNMS_HOME}/etc/
     else
-        echo "OpenNMS configuration already initialized"
+        echo "OpenNMS configuration already initialized."
     fi
 }
 start() {
@@ -97,7 +101,7 @@ shift $((OPTIND - 1));
 
 # Check if there are remaining arguments
 if [[ "${#}" > 0 ]]; then
-    echo "Error: To many arguments: ${*}"
+    echo "Error: To many arguments: ${*}."
     usage
     exit ${E_ILLEGAL_ARGS}
 fi
